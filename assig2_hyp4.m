abf3cfbb865b4f10b2e0930f7a350cfb7924d17a
ranahesham7 <47699113+ranahesham7@users.noreply.github.com>
2019-02-16 21:07:20 +0200

    Add files via upload

---
+++ assig2_hyp4.m
@@ -0,0 +1,87 @@
+clc
+clear all
+close all
+ds = datastore('heart_DD.csv','TreatAsMissing','NA',.....
+    'MissingValue',0,'ReadSize',250);
+T = read(ds);
+m= length(T{:,1});
+s=ceil(m*0.8);
+s1=ceil(m*0.1);
+x=T{1:s,1:13};
+X=[ones(s,1) x./7 x./11 x./95];
+%Y=T{1:s,14}/mean(T{1:s,14});
+Y=T{1:s,14};
+y=T{1:s,14};
+Alpha=0.01;
+lambda=100;
+
+%compute cost and gradient
+%cross validation on 80% of the data
+ n=length(X(1,:)); 
+ for w=2:n
+    if max(abs(X(:,w)))~=0
+    X(:,w)=(X(:,w)-mean((X(:,w))))./std(X(:,w));
+    end
+ end
+ theta=zeros(n,1);
+ h=1./(1+exp(-X*theta));  %sigmoid function
+ k=1;
+ J(k)=-(1/s)*sum(Y.*log(h)+(1-Y).*log(1-h))+(lambda/(2*s))*sum((theta).^2);  %cost function
+ grad=zeros(size(theta,1),1);     %gradient vector
+ for i=1:size(grad)
+     grad(i)=(1/s)*sum((h-Y)'*X(:,i));
+ end
+R=1;
+while R==1
+Alpha=Alpha*1;
+theta=theta-(Alpha/s)*X'*(h-Y);
+h=1./(1+exp(-X*theta));  %sigmoid function
+k=k+1
+J(k)=(-1/s)*sum(Y.*log(h)+(1-Y).*log(1-h))+(lambda/(2*s))*sum((theta).^2);
+if J(k-1)-J(k) <0 
+    break
+end 
+q=(J(k-1)-J(k))./J(k-1);
+if q <.00001
+    R=0;
+end
+end 
+
+ 
+%cross validation on 10% of the data
+x1=T{s+1:s+s1,1:13};
+X1=[ones(s1,1) x1./7 x1./11 x1./95];
+%Y1=T{s+1:s+s1,14}/mean(T{s+1:s+s1,14});
+y1=T{s+1:s+s1,14};
+Y1=y1;
+n1=length(X1(1,:)); 
+ for w1=2:n1
+    if max(abs(X1(:,w1)))~=0
+    X1(:,w1)=(X1(:,w1)-mean((X1(:,w1))))./std(X1(:,w1));
+    end
+ end
+ theta1=theta;
+ h1=1./(1+exp(-X1*theta1));  %sigmoid function
+ k1=1;
+ J1(k1)=-(1/s1)*sum(Y1.*log(h1)+(1-Y1).*log(1-h1))+(lambda/(2*s1))*sum((theta1).^2);  %cost function
+
+%last 10% of the data
+s2=s+s1+1;
+p=m-(s+s1);
+x2=T{s2:end,1:13};
+X2=[ones(p,1) x2./7 x2./11 x2./95];
+%Y1=T{s+1:s+s1,14}/mean(T{s+1:s+s1,14});
+y2=T{s2:end,14};
+Y2=y2;
+n2=length(X2(1,:)); 
+ for w2=2:n2
+    if max(abs(X2(:,w2)))~=0
+    X2(:,w2)=(X2(:,w2)-mean((X2(:,w2))))./std(X2(:,w2));
+    end
+ end
+ theta2=theta1;
+ h2=1./(1+exp(-X2*theta2));  %sigmoid function
+ k2=1;
+ J2(k2)=-(1/s1)*sum(Y2.*log(h2)+(1-Y2).*log(1-h2))+(lambda/(2*s1))*sum((theta2).^2);  %cost function
+ 
+ plot(J)
---
+++ machinee.m
@@ -0,0 +1,88 @@
+clc
+clear all
+close all
+ds = datastore('house_data_complete.csv','TreatAsMissing','NA',.....
+    'MissingValue',0,'ReadSize',25000);
+T = read(ds);
+size(T);
+Alpha=.01;
+m=length(T{:,1});
+s=ceil(m*0.6);
+s1=ceil(m*0.2);
+U0=T{1:s,2};
+U=T{1:s,4:19};
+U1=T{1:s,20:21};
+U2=U.^3;
+U3=U.^2;
+X=[ones(s,1) U2 U3];
+n=length(X(1,:));
+for w=2:n
+    if max(abs(X(:,w)))~=0
+    X(:,w)=(X(:,w)-mean((X(:,w))))./std(X(:,w));
+    end
+end
+Y=T{1:s,3}/mean(T{1:s,3});
+Theta=zeros(n,1);
+k=1;
+E(k)=(1/(2*s))*sum((X*Theta-Y).^2);
+R=1; %Flag
+
+%cross validation with 60%
+while R==1
+Alpha=Alpha*1;
+Theta=Theta-(Alpha/s)*X'*(X*Theta-Y);
+k=k+1
+E(k)=(1/(2*s))*sum((X*Theta-Y).^2);
+if E(k-1)-E(k)<0
+    break
+end 
+q=(E(k-1)-E(k))./E(k-1);
+if q <.001;
+    R=0;
+end
+end
+plot(E)
+
+%cross validation with 20%
+UU0=T{s+1:s+s1,2};
+UU=T{s+1:s+s1,4:19};
+UU1=T{s+1:s+s1,20:21};
+UU2=UU.^3;
+UU3=UU.^2;
+XX=[ones(s1,1) UU2 UU3 ];
+Theta1=Theta;
+YY=T{s+1:s+s1,3}/mean(T{s+1:s+s1,3});
+nn=length(XX(1,:));
+
+for w=2:nn
+    if max(abs(XX(:,w)))~=0
+    XX(:,w)=(XX(:,w)-mean((XX(:,w))))./std(XX(:,w));
+    end
+end
+kk=1;
+EE(kk)=(1/(2*s1))*sum((XX*Theta1-YY).^2);
+
+
+
+%cross validation for the last 20%
+s2=s+s1+1;
+p=m-(s+s1);
+UU0last=T{s2:end,2};
+UUlast=T{s2:end,4:19};
+UU1last=T{s2:end,20:21};
+UU2last=UUlast.^3;
+UU3last=UUlast.^2;
+
+XXlast=[ones(p,1) UU2last UU3last];
+
+Theta2=Theta1;
+YYlast=T{s2:end,3}/mean(T{s2:end,3});
+nnlast=length(XXlast(1,:));
+for w1=2:nnlast
+    if max(abs(XXlast(:,w1)))~=0
+    XXlast(:,w1)=(XXlast(:,w1)-mean((XXlast(:,w1))))./std(XXlast(:,w1));
+    end
+end
+kk2=1;
+EE2(kk2)=(1/(2*s1))*sum((XXlast*Theta2-YYlast).^2);
+
